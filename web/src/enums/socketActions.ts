8486da2d0687212b6b2dce2e9b59183f350207f8
Omer Batilović <omer.batilovic@rbt.rs>
2022-10-21 17:00:16 +0200

    Update Web to add enums

---
+++ web/src/enums/socketActions.ts
@@ -0,0 +1,8 @@
+export enum socketActions {
+  connect = "connect",
+  connectionError = "connect_error",
+  joinRoom = "join_room",
+  leaveRoom = "leave_room",
+  sendMessage = "send_message",
+  recieveMessage = "recieve_message",
+}
