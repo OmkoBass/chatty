039188ac16fcb0113be676ab9f1c70298aff001b
Omer Batilović <omer.batilovic@rbt.rs>
2022-10-21 16:33:30 +0200

    Update Backend to initialize db and migrate from extensions

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,18 @@
 from flask_socketio import SocketIO
 from flask import Flask
 from config import Config
+from . extensions import db, migrate
 
 socketio = SocketIO()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    from app.models import User, Message
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from .resources.socket import socket
     app.register_blueprint(socket)
